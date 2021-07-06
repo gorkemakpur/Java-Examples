b70906bb1ad8b1bd2ef9d3244d990c9a90b3da71
gorkemakpur <gorkemakpur@gmail.com>
2021-07-06 14:45:26 +0300

    .

---
+++ recapDemo_Classes/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	dortIslem dortIslem=new dortIslem();
+	double a=dortIslem.topla(45,40);
+	double b=dortIslem.cikar(33,3);
+	double c=dortIslem.bol(30,3);
+	double d=dortIslem.carp(3,10);
+        System.out.println(a+"\n"+b+"\n"+c+"\n"+d);
+    }
+}
---
+++ recapDemo_Classes/src/com/company/dortIslem.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class dortIslem {
+    public double topla(double a,double b)
+    {
+        return a+b;
+    }
+
+    public double cikar(double a,double b)
+    {
+        return a-b;
+    }
+
+    public double carp(double a,double b)
+    {
+        return a*b;
+    }
+
+    public double bol(double a,double b)
+    {
+        return a/b;
+    }
+
+}
