440b71e0d77dbdd5be2621c218887ed21b96dc17
gorkemakpur <gorkemakpur@gmail.com>
2021-07-26 13:25:33 +0300

    update-Workout

---
+++ jdbcIntro/src/com/company/Country.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Country
+{
+    String Code;
+    String Name;
+    String Continent;
+    String Region;
+
+    public Country(String Code,String Name,String Continent, String Region)
+    {
+        this.Code=Code;
+        this.Name=Name;
+        this.Continent=Continent;
+        this.Region=Region;
+
+    }
+}
---
+++ jdbcIntro/src/com/company/DbHelper.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbHelper {
+    String userName = "root";
+    String password = "123456";
+    String dbUrl = "jdbc:mysql://localhost:3306/world";
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(dbUrl, userName, password);
+    }
+
+    public void showErrorMessage(SQLException exception) {
+        System.out.println("Error:" + exception.getMessage());
+        System.out.println("Error Code:" + exception.getErrorCode());
+    }
+}
